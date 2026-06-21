01540fa69bbbf201db5c26c07cdaf5aae45dd9c8
Shashankkharadi <166908324+Shashankkharadi@users.noreply.github.com>
2026-06-21 12:27:56 +0530

    Update MockTest.js

--- backend/models/MockTest.js
+++ backend/models/MockTest.js
@@ -1,1 +1,20 @@
+const mongoose = require("mongoose");
 
+const MockTestSchema = new mongoose.Schema({
+
+ userId:String,
+
+ exam:String,
+
+ subject:String,
+
+ questions:[Object],
+
+ score:Number,
+
+ duration:Number
+
+});
+
+module.exports =
+mongoose.model("MockTest",MockTestSchema);
