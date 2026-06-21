437c7e1f43fe98e0af8f9b1e18da55b233faf928
Shashankkharadi <166908324+Shashankkharadi@users.noreply.github.com>
2026-06-21 12:19:11 +0530

    Update create-test.js

--- frontend/pages/create-test.js
+++ frontend/pages/create-test.js
@@ -1,1 +1,41 @@
+import { useState } from "react";
 
+export default function CreateTest() {
+
+ const [form,setForm] = useState({
+   exam:"",
+   class:"",
+   subject:"",
+   chapter:"",
+   difficulty:"",
+   duration:""
+ });
+
+ return (
+   <div className="p-10">
+
+     <h1 className="text-3xl font-bold">
+       Create Mock Test
+     </h1>
+
+     <select>
+       <option>JEE Main</option>
+       <option>JEE Advanced</option>
+       <option>NEET</option>
+     </select>
+
+     <select>
+       <option>Class 11</option>
+       <option>Class 12</option>
+     </select>
+
+     <select>
+       <option>Physics</option>
+       <option>Chemistry</option>
+       <option>Maths</option>
+       <option>Biology</option>
+     </select>
+
+   </div>
+ );
+}
