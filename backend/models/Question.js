722509bcdc6e54572324af00b535d185d81293d9
Shashankkharadi <166908324+Shashankkharadi@users.noreply.github.com>
2026-06-21 12:24:51 +0530

    Update Question.js

--- backend/models/Question.js
+++ backend/models/Question.js
@@ -1,1 +1,30 @@
+const mongoose = require("mongoose");
 
+const QuestionSchema = new mongoose.Schema({
+
+ exam:String,
+
+ className:String,
+
+ subject:String,
+
+ chapter:String,
+
+ topic:String,
+
+ difficulty:String,
+
+ question:String,
+
+ options:[String],
+
+ answer:String,
+
+ solution:String,
+
+ source:String
+
+});
+
+module.exports =
+mongoose.model("Question",QuestionSchema);
